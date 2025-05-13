704c62d2ba40153607a4ef89de4bc667cbe66c3a
Pratap Padhy <pratappadhy2005@gmail.com>
2025-05-14 08:21:09 +1000

    -Added error handling with asyncc handler

--- 06-CONTACT-MANAGER/express-project/mycontacts-backend/server.js
+++ 06-CONTACT-MANAGER/express-project/mycontacts-backend/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const contactRoute = require("./routes/contactRoute");
 const app = express();
+const errorHandler = require("./middleware/errorHandler");
 
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/contacts", contactRoute)
+app.use(express.json());
+app.use("/api/contacts", contactRoute);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
