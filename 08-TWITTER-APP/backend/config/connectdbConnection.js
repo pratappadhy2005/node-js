2e934ec1d65eb4c827c03a8aedc11a48b322fba0
Pratap Padhy <pratappadhy2005@gmail.com>
2025-06-03 19:56:33 +1000

    -Added DB connection

---
+++ 08-TWITTER-APP/backend/config/connectdbConnection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const dbconnection = async () => {
+    try {
+        console.log("Connecting to the database" + process.env.CONNECTION_STRING);
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        console.log("Connected to the database", connect.connection.host, connect.connection.name);
+    } catch (error) {
+        console.log("Error connecting to the database", error);
+    }
+}
+
+export default dbconnection;
--- 08-TWITTER-APP/backend/server.js
+++ 08-TWITTER-APP/backend/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js"
+import env from "dotenv";
+import dbconnection from "./config/connectdbConnection.js";
 
+env.config();
 const app = express();
 const port = 3000;
+dbconnection();
 
 app.use(express.json());
 app.use("/api/auth", authRoutes)
 
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Example app listening on port ${port}`);
 })
