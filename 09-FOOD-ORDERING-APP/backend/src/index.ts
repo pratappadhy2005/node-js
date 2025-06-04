bb098af82094561ff8e0d39fb23870e079bdee66
Pratap Padhy <pratappadhy2005@gmail.com>
2025-06-04 19:06:31 +1000

    -Added a new food ordering app

---
+++ 09-FOOD-ORDERING-APP/backend/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv"
+
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
