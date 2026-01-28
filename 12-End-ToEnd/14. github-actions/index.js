817a03eb066f0b11b91787382ea7202716195ebe
Pratap Padhy <pratappadhy2005@gmail.com>
2026-01-28 22:38:45 +1100

    -Added github action example

---
+++ 12-End-ToEnd/14. github-actions/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello World!"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
